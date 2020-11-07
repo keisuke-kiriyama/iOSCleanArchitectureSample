79c54bf824a56c8160adc2ae68c52f845f879894
kiriyama-keisuke <kiriyama_keisuke@r.recruit.co.jp>
2020-11-07 18:57:51 +0900

    first commit

---
+++ iOSCleanArchitectureSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSCleanArchitectureSample
+//
+//  Created by RLS01031792 on 2020/11/07.
+//  Copyright © 2020 RLS01031792. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
